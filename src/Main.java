20f1e7e9966a0f8a78efb05d37cc1e7cee4cf35e
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-02-06 17:51:28 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int price = 5678; // стоимость билета
         int mile = 200; // стоимость 1 мили
-        int amount_of_miles = price / mile; // количество миль
+        int amountOfMiles = price / mile; // количество миль
 
-        System.out.println((("На ваш счет будет зачислено ") + (amount_of_miles) + (" миль")));
+        System.out.println((("На ваш счет будет зачислено ") + (amountOfMiles) + (" миль")));
     }
 }
