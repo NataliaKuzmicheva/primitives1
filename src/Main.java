26542ff64bea1798c6b56628f962d6126381fa2a
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-02-13 15:27:07 +0300

    Change mile from 200 to 20

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int price = 5678; // стоимость билета
-        int mile = 200; // стоимость 1 мили
+        int mile = 20; // стоимость 1 мили
         int amountOfMiles = price / mile; // количество миль
 
         System.out.println((("На ваш счет будет зачислено ") + (amountOfMiles) + (" миль")));
